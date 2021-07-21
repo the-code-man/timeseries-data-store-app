b7f1151d66e7490b1c48b03c67f96d52ff1a0fc9
Rakesh Chandran <rakesh8182.cloud@gmail.com>
2021-07-21 09:51:38 +0530

    WIP - Signalr implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TimeSeriesDataComponent } from './time-series-data/time-series-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeSeriesDataComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
